e4391d0869abca2a04f4083eea27b6b8cd4f3b06
Pavan Suthar <sutharpavan9153@gmail.com>
2024-07-01 23:24:21 +0530

    new commit

--- flour_pack_problem.java
+++ flour_pack_problem.java
@@ -1,6 +1,6 @@
 public class flour_pack_problem {
     public static void main(String[] args) {
-        System.out.println(canPack(1,0,5));
+        System.out.println(canPack(1,0,9));
     }
         public static boolean canPack(int bigCount, int smallCount, int goal){
             if( bigCount < 0 || smallCount < 0 || goal < 0){
